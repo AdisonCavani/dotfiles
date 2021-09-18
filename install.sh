170fb7bf3d1e441170b67264a64d6d712dbb4c4f
Adison Cavani <adrian.srodon@gmail.com>
2021-09-18 12:36:17 +0200

    Add images

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
 #! /bin/bash
 
+# Load gnome-terminal profile
 dconf load /org/gnome/terminal/legacy/profiles:/:3c3e9364-aade-4e49-9086-31f4b686baa5/ < gnome-terminal/adison-theme-profile.dconf
+
+# Create directories for images
+mkdir -p ~/Pictures/Wallpapers/
+mkdir -p ~/Pictures/Others/
+
+cp -r img/gt3-rs.jpg ~/Pictures/Wallpapers/
+cp -r img/Adison.jpg ~/Pictures/Others/
+
+# Create directory for Keepass database
+mkdir -p ~/Documents/KeepassXC/
